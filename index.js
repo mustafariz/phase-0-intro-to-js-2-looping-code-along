3ff1a748fe00516808cfffc44636c4c41e650fa8
syed mustafa ali rizvi <mustafariz15@gmail.com>
2022-07-28 20:32:55 -0700

    commit tree 62af69eb764a315b2a2e4c4f3c8e4a5c3793801d

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(array,string){
+  const newArray =[]// empty array 
+  for(let i=0; i <array.length ;i++ ){
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+
+  }
+  console.log(newArray);
+return newArray;
+}
+
+function countDown(num){
+  while( num >=0 ){
+  console.log(num);
+  num--;
+  }
+  
+}
